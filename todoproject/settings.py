2cfe142f5eb24d1510e936c81ed0ad92ced92fe3
anshidnm <anshidnm@gmail.com>
2022-03-25 10:34:41 +0530

    ready for hosting

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'staticfiles')
 ]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
